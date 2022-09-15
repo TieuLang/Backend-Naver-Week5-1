d075632fcbc5130c1703487b79145774e88556d5
Nguyễn Quyết Thắng <103924927+ThangNguyenQuyet@users.noreply.github.com>
2022-09-15 16:01:50 +0700

    Initial commit

---
+++ driver/src/main/java/com/likelion/driver/DriverApplication.java
@@ -0,0 +1,12 @@
+package com.likelion.driver;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class DriverApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(DriverApplication.class,args);
+    }
+}
